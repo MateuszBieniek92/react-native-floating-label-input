50730c14d09363a0e458dff63dbaef2b16655b1a
Mateusz Bieniek <bieniek.mateusz92@gmail.com>
2020-07-30 09:59:46 +0200

    Update styles.tsx

--- src/styles.tsx
+++ src/styles.tsx
@@ -1,24 +1,33 @@
 import {StyleSheet} from 'react-native';
 
 export const styles = StyleSheet.create({
-  input: {
-    paddingVertical: 0,
-    paddingHorizontal: 30,
-    flex: 1,
-    zIndex: 1,
-  },
-  img: {
-    width: 20,
-    height: 20,
-    alignSelf: 'center',
-  },
-  containerInput: {
-    flexDirection: 'row',
-  },
-  toggleButton: {
-    marginRight: 15,
-    zIndex: 4,
-    alignSelf: 'center',
-    justifyContent: 'flex-end',
-  },
+    input: {
+        paddingVertical: 0,
+        flex: 1,
+        zIndex: 1,
+        color: '#000000',
+        left: -3,
+    },
+    noBorder: {
+      borderBottomWidth: 0,
+    },
+    grayText: {
+      color: '#7F7F7F'
+    },
+    img: {
+        width: 20,
+        height: 20,
+        alignSelf: 'center',
+    },
+    containerInput: {
+        flexDirection: 'row',
+        paddingTop: 5,
+        paddingBottom: 5,
+    },
+    toggleButton: {
+        marginRight: 15,
+        zIndex: 4,
+        alignSelf: 'center',
+        justifyContent: 'flex-end',
+    },
 });
